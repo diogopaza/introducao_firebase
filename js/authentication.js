ce471de3e0b63ecc1d7132cf12d13254661a150f
diogopaza <diogopazacvel@gmail.com>
2019-02-21 22:38:07 -0300

    authentication.js

---
+++ js/authentication.js
@@ -0,0 +1,13 @@
+//inputs
+emailInput = document.getElementById('emailInput')
+passwordInput = document.getElementById('passwordInput')
+//Buttons
+authEmailPassButton = document.getElementById('authEmailPassButton')
+createUserButton = document.getElementById('createUserButton')
+authGoogleButton = document.getElementById('authGoogleButton')
+authGitHubButon = document.getElementById('authGitHubButon')
+authFacebookButton = document.getElementById('authFacebookButton')
+authTwitterButton = document.getElementById('authTwitterButton')
+authAnonymouslyButton = document.getElementById('authAnonymouslyButton')
+logOutButton = document.getElementById('logOutButton')
+
