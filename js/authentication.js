cd6510fb0a3a339a37cda128077c8dbcced19f50
diogopaza <diogopazacvel@gmail.com>
2019-02-28 21:03:18 -0300

    auth

--- js/authentication.js
+++ js/authentication.js
@@ -11,3 +11,20 @@ authTwitterButton = document.getElementById('authTwitterButton')
 authAnonymouslyButton = document.getElementById('authAnonymouslyButton')
 logOutButton = document.getElementById('logOutButton')
 
+displayName = document.getElementById('displayName')
+
+createUserButton.addEventListener('click', function(){
+    
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword( emailInput.value, passwordInput.value )
+        .then( function(){
+            alert(" Bem vindo " + emailInput.value )
+        })
+        .catch(function(error){
+            console.error(error.code)
+            console.error(error.message)
+            alert('Falha ao cadastrar, verifique o erro no console')
+        })
+
+})
