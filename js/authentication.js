25b748b0b87a25d65f2d8ab5bbb4278f1b276061
diogopaza <diogopazacvel@gmail.com>
2019-03-02 20:37:58 -0300

    cap10 finishing

--- js/authentication.js
+++ js/authentication.js
@@ -31,6 +31,7 @@ createUserButton.addEventListener('click', function(){
 
 authEmailPassButton.addEventListener('click', function(){
 
+ 
     firebase
         .auth()
         .signInWithEmailAndPassword(emailInput.value, passwordInput.value)
@@ -45,6 +46,8 @@ authEmailPassButton.addEventListener('click', function(){
             console.error(error.message)
             alert('Falha ao autenticar, verifique o erro no console')
         })
+        
+    
 })
 
 logOutButton.addEventListener('click', function(){
@@ -59,3 +62,55 @@ logOutButton.addEventListener('click', function(){
             console.log(error)
         })
 })
+
+authAnonymouslyButton.addEventListener('click', function(){
+
+    firebase
+        .auth()
+        .signInAnonymously()
+        .then(function(result){
+            console.log(result)
+            displayName.innerText = "Bem vindo desconhecido" 
+            alert('Autenticado Anonimamente' ) 
+        })
+        .catch(function(error){
+            console.error(error.code)
+            console.error(error.message)
+            alert('Falha ao autenticar, verifique o erro no console')
+        })
+})
+
+
+authGitHubButon.addEventListener('click', function(){
+    var provider = new firebase.auth.GithubAuthProvider()
+        signIn(provider)
+})
+
+authGoogleButton.addEventListener('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider()
+        signIn(provider)
+})
+
+authFacebookButton.addEventListener('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider()
+        signIn(provider)
+})
+
+
+
+function signIn( provider ){
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function(result){
+            console.log(result)
+            var token = result.credential.accessToken
+            displayName.innerText = 'Bem vindo '+ result.user.displayName
+
+        })
+        .catch(function(error){
+            console.log(error)
+            alert('Falha na autenticação')
+
+        })
+}
+
