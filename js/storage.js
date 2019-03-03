357353a5b8476fc221f32e82b1932d576fe87a55
diogopaza <diogopazacvel@gmail.com>
2019-03-02 23:44:40 -0300

    storage finished

--- js/storage.js
+++ js/storage.js
@@ -5,5 +5,24 @@ fileButton = document.getElementById( 'fileButton' )
 
 fileButton.addEventListener('change', function(e){
     var file = e.target.files[0]
-    console.log( file )
-})
+    //Referenciar o storage
+    var storageRef = firebase.storage().ref('arquivos/'+file.name)
+    var task = storageRef.put(file)
+   
+    //Atualizar o progress
+    task.on('state_changed',
+        function progress(snapshot){
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes ) * 100
+            uploader.value = percentage
+        },
+        function(err){
+            console.log(err)
+        },
+        function complete(){
+            alert('envio completo')
+        } 
+
+    )
+
+        })
+    
